c8e2ccff13a249f32b09926c2f43749366e0a08f
medrauty <113467872+medrauty@users.noreply.github.com>
2023-05-16 16:52:56 -0300

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,16 @@
-function toggle(){
+function toggleBulbasaur() {
     var bulbasaurBtn = document.getElementById('bulbasaur-btn');
     bulbasaurBtn.classList.toggle('active')
 
-    var popupBulbasaur =document.getElementById('popup-bulbasaur');
-    popupBulbasaur.classList.toggle('active')
+    var popupBulbasaur = document.getElementById('popup-bulbasaur');
+    popupBulbasaur.classList.toggle('bulbasaur')
 }
+
+function toggleIvysaur() {
+    var ivysaurBtn = document.getElementById('ivysaur-btn');
+    ivysaurBtn.classList.toggle('active')
+
+    var popupIvysaur = document.getElementById('popup-ivysaur');
+    popupIvysaur.classList.toggle('ivysaur')
+}
+
