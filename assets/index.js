fecd19389ff8363e240d76926b7d05225e3f0432
medrauty <113467872+medrauty@users.noreply.github.com>
2023-05-17 00:05:17 -0300

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -1,15 +1,79 @@
 function toggleBulbasaur() {
-    var bulbasaurBtn = document.getElementById('bulbasaur-btn');
-    bulbasaurBtn.classList.toggle('active')
+    var blur=document.getElementById('blur');
+    blur.classList.toggle('active');
 
     var popupBulbasaur = document.getElementById('popup-bulbasaur');
-    popupBulbasaur.classList.toggle('bulbasaur')
+    popupBulbasaur.classList.toggle('bulbasaur');
 }
-
+//Ivysaur//
 function toggleIvysaur() {
-    var ivysaurBtn = document.getElementById('ivysaur-btn');
-    ivysaurBtn.classList.toggle('active')
+    var blur=document.getElementById('blur');
+    blur.classList.toggle('active');
 
     var popupIvysaur = document.getElementById('popup-ivysaur');
-    popupIvysaur.classList.toggle('ivysaur')
+    popupIvysaur.classList.toggle('ivysaur');
+}
+//Venusaur
+function toggleVenusaur() {
+    var blur=document.getElementById('blur');
+    blur.classList.toggle('active');
+
+    var popupVenusaur = document.getElementById('popup-venusaur');
+    popupVenusaur.classList.toggle('venusaur');
+}
+//Charmander
+function toggleCharmander() {
+    var blur=document.getElementById('blur');
+    blur.classList.toggle('active');
+
+    var popupCharmander = document.getElementById('popup-charmander');
+    popupCharmander.classList.toggle('charmander');
+}
+//Charmeleon
+function toggleCharmeleon() {
+    var blur=document.getElementById('blur');
+    blur.classList.toggle('active');
+
+    var popupCharmeleon = document.getElementById('popup-charmeleon');
+    popupCharmeleon.classList.toggle('charmeleon');
+}
+//Charizard
+function toggleCharizard() {
+    var blur=document.getElementById('blur');
+    blur.classList.toggle('active');
+
+    var popupCharizard = document.getElementById('popup-charizard');
+    popupCharizard.classList.toggle('charizard');
+}
+//Squirtle
+function toggleSquirtle() {
+    var blur=document.getElementById('blur');
+    blur.classList.toggle('active');
+
+    var popupSquirtle = document.getElementById('popup-squirtle');
+    popupSquirtle.classList.toggle('squirtle');
+}
+//Wartortle
+function toggleWartortle() {
+    var blur=document.getElementById('blur');
+    blur.classList.toggle('active');
+
+    var popupWartortle = document.getElementById('popup-wartortle');
+    popupWartortle.classList.toggle('wartortle');
+}
+//Blastoise
+function toggleBlastoise() {
+    var blur=document.getElementById('blur');
+    blur.classList.toggle('active');
+
+    var popupBlastoise = document.getElementById('popup-blastoise');
+    popupBlastoise.classList.toggle('blastoise');
+}
+//Caterpie
+function toggleCaterpie() {
+    var blur=document.getElementById('blur');
+    blur.classList.toggle('active');
+
+    var popupCaterpie = document.getElementById('popup-caterpie');
+    popupCaterpie.classList.toggle('caterpie');
 }
